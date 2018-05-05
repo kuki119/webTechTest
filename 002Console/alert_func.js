f22d42466d26c909ff3b3e75ab21fed8a67cb5d4
kuki119 <1458369448@qq.com>
2018-05-05 20:23:52 +0800

    make a js alert function

---
+++ 002Console/alert_func.js
@@ -0,0 +1,9 @@
+function func(){
+    alert('I am a genius！！')
+    alert('You guess what?!?')
+    alert('I am not only smart, I am handsome too!!')
+}
+// ctrl + /  注释  ctrl + shift + /
+alert('*********')
+func()
+alert('############')
