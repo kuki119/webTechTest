0b86f5f2858c23b1003a2b77fd2a0b59d032c8a6
kuki119 <1458369448@qq.com>
2018-05-05 20:59:53 +0800

    add some basic calculations

--- 002Console/alert_func.js
+++ 002Console/alert_func.js
@@ -4,6 +4,39 @@ function func(){
     alert('I am not only smart, I am handsome too!!')
 }
 // ctrl + /  注释  ctrl + shift + /
-alert('*********')
-func()
-alert('############')
+// alert('*********')
+// func()
+// alert('############')
+
+//做一点计算：
+// var prices = 1000;
+// var discount = prompt('You want how many of discount??');
+// alert('congratulations！now the price is '+ prices*discount);
+
+//做一点逻辑运算
+// var passwd = prompt('please input your password:')
+// if (passwd == 23561411){
+//     alert('welcome back!!')
+// }
+// else if (passwd == 123456){
+//     alert('You can only read a little of information')
+// }
+// else{
+//     alert('the password is wrong!!please check again!!')
+// }
+
+var age = prompt('Input your age:')
+if (isNaN(age/2)){
+    alert('please input a number')
+}
+else if (age>=18){
+    if (age>200){
+        alert('Are you serious??')
+    }
+    else{
+        alert('Congratulations!')
+    }
+}
+else{
+    alert('A fucking child....')
+}
